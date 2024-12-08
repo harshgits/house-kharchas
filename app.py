c7f3d2ec14cba16b5e21939e9de6fbbdaef646c8
Harsh Chaturvedi <chaturhar@gmail.com>
2024-12-08 20:00:04 +0000

    added html output

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def input_form():
-    ownership_table_new_text = None  # Initialize the result as None
+
+    # initialize form inputs
+    ownership_table_new_text = None
+    ownership_table_new_html = None
+
     if request.method == "POST":
+        
         # Get the input values from the form
         undocd_kharchas = request.form["undocd_kharchas"]
         ownership_table = request.form["ownership_table"]
@@ -15,17 +20,23 @@ def input_form():
 
         # compute result
         try:
-            ownership_table_new_text, extras = OTT.ingest_undocumented_kharchas_to_ownership_table(
-                ownership_table, undocd_kharchas, rebuild_table
+            ownership_table_new_text, extras = (
+                OTT.ingest_undocumented_kharchas_to_ownership_table(
+                    ownership_table, undocd_kharchas, rebuild_table
+                )
             )
             ownership_table_new_html = extras["o_htmltable_new"]
         except Exception as e:
             ownership_table_new_text = f"Error occured during ingestion: \n{e}"
 
     # Render the template, pass the concatenated result to the template
-    return render_template("webpage.html", ownership_table_new=ownership_table_new_text)
+    return render_template(
+        "webpage.html",
+        ownership_table_new_text=ownership_table_new_text,
+        ownership_table_new_html=ownership_table_new_html,
+    )
 
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", port=5000) # use this line when using docker
-    app.run(debug=True) # use this line when debugging the Flask app directluy
+    app.run(debug=True)  # use this line when debugging the Flask app directluy
